c64c778db84080ba7f91de2df4a8e4584fd6ec14
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-09-29 02:00:59 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
 import { SessionProvider } from 'next-auth/react'
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import createEmotionCache from '../custom-modules/createEmotionCache';
+import lightTheme from '../styles/theme/lightTheme';
+import '../styles/globals.css';
+const clientSideEmotionCache = createEmotionCache();
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }) {
+
     return (
         <SessionProvider session={pageProps.session}>
-            <Component {...pageProps} />
+            <CacheProvider value={emotionCache}>
+                <ThemeProvider theme={lightTheme}>
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </CacheProvider>
         </SessionProvider>
     )
 }
