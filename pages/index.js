5c14efd798c911c6c88269ea8e51a167e0b8145a
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-09-29 18:38:46 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,26 @@ import BingMapsReact from "../custom-modules/bingmaps-react";
 import React, { useEffect, useState } from "react";
 import dotenv from "dotenv";
 import { mapClickEvent } from "bingmaps-react";
-console.log(mapClickEvent)
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import SendIcon from '@mui/icons-material/Send';
 dotenv.config();
+const options = [
+  {
+    value: 'yes',
+    label: 'Yes',
+  },
+  {
+    value: 'no',
+    label: 'No',
+  }
+];
 
 export default function Home() {
   const [pushPins, setpushPins] = useState([]);
@@ -23,6 +41,17 @@ export default function Home() {
       setpushPins(currentList => [...currentList, pushPin]);
     });
   }, []);
+  const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
   if (pushPins.length === 0) {
     return <div>Loading...</div>;
   }
@@ -39,39 +68,87 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
         <BingMapsReact pushPins={pushPins} height="80vh" viewOptions={{ center: center, mapTypeId: 'aerial', zoom: 19 }} bingMapsKey={process.env.NEXT_PUBLIC_API} />
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 2 }, textAlign: 'center'
+          }}
+          noValidate
+          autoComplete="off"
+        >
+       
+          <TextField
+            id="emergency-text"
+            label="What's the Emergency?"
+            multiline
+            maxRows={4}
+            sx={{ width: '45vw' }}
+          />
+          <TextField
+            id="location-text"
+            label="Where are you?"
+            multiline
+            maxRows={4}
+            sx={{ width: '45vw' }}
+          />
+          <br />
+          <TextField
+            id="additional-info"
+            label="Additional Information"
+            multiline
+            sx={{ width: '45vw' }}
+          />
+          <TextField
+            id="call-emergency"
+            select
+            label="Select"
+            sx={{ width: '22vw' }}
+            helperText="Did you called emergency services?"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            {options.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="call-amulance"
+            select
+            label="Select"
+            sx={{ width: '20vw' }}
+            helperText="Do you need an ambulance?"
           >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            {options.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <div style={{display:'flex',flexDirection:'row',justifyContent:'space-around'}}>
+          <div style={{display:'flex',flexDirection:'column',justifyContent:'space-around'}}>
+              <Typography id="discrete-slider"  gutterBottom>Seriousness of the emergency ?</Typography>
+          
+          <Slider
+            aria-label="Seriousness of the emergency ?"
+            defaultValue={1}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+                sx={{ color: 'success.main'}}
+            min={1}
+            max={5}
+          />
+            </div>
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+              Upload file
+              <VisuallyHiddenInput type="file" />
+            </Button>
+          </div>
+          <Button variant="contained" endIcon={<SendIcon />} size='large' sx={{marginTop:3}}>
+            Submit
+          </Button>
+        </Box>
+        
       </main>
 
       <footer>
