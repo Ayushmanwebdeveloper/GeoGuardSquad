a1d651cfb3c52ba2563999add25d31daefcb49b1
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-10-01 02:23:26 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,21 @@ export default function Home({ incidents }) {
   let session;
   const uniqueKey = Date.now();
   console.log(incidents);
+  
+  useEffect(() => {
+    const checkSession = async () => {
+      session = await getSession();
+      console.log(session);
+      if (session?.user?.name) {
+        setIsDisabled(false);
+        setUser(session.user);
+      } else {
+        setIsDisabled(true);
+      }
+    };
 
+    checkSession();
+  }, []);
 
     useEffect(() => {
       navigator.geolocation.getCurrentPosition(function (position) {
