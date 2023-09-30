99f7a7e01d6a3b43fe90c26c523ce3b54ffc1ce6
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-10-01 01:37:43 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,18 +14,5 @@ module.exports = {
                 }
             ],
         },
-        {
-            source: '*',
-            headers: [
-                {
-                    key: 'Cache-Control',
-                    value: 'no-store'
-                },
-                {
-                    key: 'Clear-Site-Data',
-                    value: '*',
-                }
-            ],
-        },
     ],
 }
