1012c39af815d5eda35f1734e9a70cfabc80105b
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-09-30 22:41:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -139,7 +139,7 @@ export default function Home({ incidents }) {
           Welcome to Geo Guard Squad
         </h1>
         <DynamicBingMap pushPins={pushPins} height="80vh" viewOptions={{ center: center, mapTypeId: 'aerial', zoom: 19 }} bingMapsKey={process.env.NEXT_PUBLIC_API} />
-        <BingMapsReact pushPins={pushPins} height="80vh" viewOptions={{ center: center, mapTypeId: 'aerial', zoom: 19 }} bingMapsKey={process.env.NEXT_PUBLIC_API} />
+        
         <Box
           component="form"
           sx={{
