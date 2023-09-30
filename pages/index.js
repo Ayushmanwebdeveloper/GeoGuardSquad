a396a257407f769f6b537ad3efd0c9f432ad21fd
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-09-30 13:51:36 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -53,20 +53,7 @@ export default function Home({ incidents }) {
     });
   }, []);
 
-  useEffect(() => {
-    const checkSession = async () => {
-      const session = await getSession();
-      console.log(session);
-      if (session.user.name) {
-        setIsDisabled(false);
-      } else {
-        setIsDisabled(true);
-      }
-    };
 
-    checkSession();
-  }, [session]);
-  
   const VisuallyHiddenInput = styled('input')({
     clip: 'rect(0 0 0 0)',
     clipPath: 'inset(50%)',
