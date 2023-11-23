4adaffc0b4089ecdc89a503f1be626339513a20c
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-11-24 00:56:32 +0530

    Update createMsg.js

--- pages/api/createMsg.js
+++ pages/api/createMsg.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         let result=clientMsg.messages
             .create({
                 from: 'whatsapp:+14155238886',
-                body: `A new incident has been reported at ${sessionStorage.getItem('latitude') + ',' + sessionStorage.getItem('longitude')}`,
+                body: `A new incident has been reported. Please check the dashboard for more details.`,
                 to: 'whatsapp:+916006188367'
             })
             .then(message => console.log(message.sid));
