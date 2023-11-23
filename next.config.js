4ba43feb948afce5667dba8bd146a65b486b10d6
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-11-23 23:39:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,18 @@ module.exports = {
                     value: '*',
                 }
             ],
-        },
+        }
     ],
+    webpack(config) {
+        config.resolve.fallback = {
+
+            // if you miss it, all the other options in fallback, specified
+            // by next.js will be dropped.
+            ...config.resolve.fallback,
+
+            fs: false, // the solution
+        };
+
+        return config;
+    },
 }
