d8e8b52a3772493de22e88a67c0dd7b64e9719c0
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-11-24 00:14:25 +0530

    update

---
+++ pages/api/createMsg.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+    try {
+        const accountSid = process.env.TWILIO_ACCOUNT_SID;
+        const authToken = process.env.TWILIO_AUTH_TOKEN;
+        const clientMsg = require('twilio')(accountSid, authToken);
+        console.log("cm" + clientMsg);
+        clientMsg.messages
+            .create({
+                from: 'whatsapp:+18327304102',
+                body: 'Your incident has been submitted successfully',
+                to: 'whatsapp:+916006157825'
+            })
+            .then(message => console.log(message.sid));
+    }
+    catch (e) {
+        console.error(e);
+    }
+};
